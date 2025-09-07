b79ec82720ce440c7925ee87d332d57301501a80
ollily <426229+ollily@users.noreply.github.com>
2025-09-07 15:02:27 +0200

    #3: Traits added

---
+++ src/Tools/String/ImplodeTrait.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ezlogging
+ *
+ * (c) 2025 Oliver Glowa, coding.glowa.com
+ *
+ * This source file is subject to the Apache-2.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace oglowa\tools\Yacorapi\Helper;
+
+trait ImplodeTrait
+{
+    /**
+     * Recursively implodes an array with optional key inclusion.
+     *
+     * Example of $include_keys output: key, value, key, value, key, value
+     *
+     * @param string $separator    value that glues elements together
+     * @param mixed  $anyData      multi-dimensional array to recursively implode
+     * @param bool   $display_keys include keys before their values
+     *
+     * @return string imploded array
+     */
+    function arrayRecImplode(string $separator, $anyData, bool $display_keys = false): string
+    {
+        $output      = '';
+        $value_index = 0;
+        if (is_array($anyData) || is_object($anyData)) {
+            foreach ($anyData as $key => $value) {
+                $output .= ($value_index ? $separator : '') . ($display_keys ? (is_int($key) ? $key : "'" . $key . "'") . '=>' : '');
+                if (is_array($value)) {
+                    $array_ouput = $this->arrayRecImplode($separator, $value, $display_keys);
+                    if ($array_ouput) {
+                        $output .= '[' . $array_ouput . ']';
+                    } else {
+                        $output .= '[]';
+                    }
+                } else {
+                    if (is_object($value)) {
+                        $object_output = $this->arrayRecImplode($separator, $value, $display_keys);
+                        if ($object_output) {
+                            $output .= '{' . $object_output . '}';
+                        } else {
+                            $output .= '{}';
+                        }
+                    } else {
+                        $output .= '"' . $value . '"';
+                    }
+                }
+                $value_index++;
+            }
+        } else {
+            $output = $anyData;
+        }
+
+        return $output;
+    }
+}
---
+++ src/Tools/String/ToStringTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ezlogging
+ *
+ * (c) 2025 Oliver Glowa, coding.glowa.com
+ *
+ * This source file is subject to the Apache-2.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace ollily\Tools;
+
+use oglowa\tools\Yacorapi\Helper\ImplodeTrait;
+
+trait ToStringTrait
+{
+    use ImplodeTrait;
+
+    abstract function __toStringValues(): array;
+
+    /**
+     * @inheritdoc
+     */
+    public function __toString()
+    {
+        return sprintf('[{%s}:{%s}]', get_class($this), $this->arrayRecImplode(',', $this->__toStringValues(), true));
+    }
+}
